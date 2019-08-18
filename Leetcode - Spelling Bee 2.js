99f5efa806d827ab08907eb9f6eab030ae85c359
phongnguyen39 <phong.nguyen39@gmail.com>
2019-08-17 18:37:22 -0700

    in progress LC - Spelling Bee.js

--- Leetcode - Spelling Bee 2.js
+++ Leetcode - Spelling Bee 2.js
@@ -1,9 +1,5 @@
 // https://leetcode.com/discuss/interview-question/354523/dropbox-oa-2019-spelling-bee
 
-// OBJECTIVE:  How many words from the wordlist be formed by using the puzzles?
-// each puzzle 7 distinct char, the first char is the'key'
-// come up with as many words as possible:  english, five letters long, contains key, no letters outside of 7 letters, letters may be reused including the key
-
 function spellingBeeSolutions(wordlist, puzzles) {
     let wordDict = {};
     let output = [];
@@ -18,13 +14,11 @@ function spellingBeeSolutions(wordlist, puzzles) {
         let puzzLetter = puzzles[p].split('')
         console.log('puzzle', puzzLetter);
         let count = 0;
+        let microCounter = 0;
 
-        for (let z = 0; z < wordlist.length; z++) {
-            let microCounter = 0;
-            let overcount = 0;
-            for (let q = 0; q < puzzLetter.length; q++) {
+        for (let q = 0; q < puzzLetter.length; q++) {
 
-
+            for (let z = 0; z < wordlist.length; z++) {
                 // IF STATEMENT BREAKS IF NO KEY LETTER
                 if (!wordDict[wordlist[z]].has(puzzLetter[0])) {
                     // console.log('keyletter', puzzLetter[0], wordDict[wordlist[z]]);
@@ -34,15 +28,20 @@ function spellingBeeSolutions(wordlist, puzzles) {
                 // console.log('wordList', wordlist, 'puzlett', puzzLetter[q]);
 
                 // CHECKS IF PUZZLE LETTER EXIST IN WORD
-                // console.log(wordDict[wordlist[z]].has(puzzLetter[q]), wordDict[wordlist[z]], puzzLetter[q])
-                overcount++;
                 if (wordDict[wordlist[z]].has(puzzLetter[q])) {
                     microCounter++;
+                    
+                    console.log(wordDict[wordlist[z]].has(puzzLetter[q]), Math.round(microCounter/3))
+                    // wordDict[wordlist[z]].delete(puzzLetter[q]);
+                    console.log(true, wordDict[wordlist[z]], puzzLetter[q])
                 }
-
-                if (microCounter >= wordDict[wordlist[z]].size && q === puzzLetter.length - 1) {
+                // console.log(wordDict[wordlist[z]]);
+                if (microCounter == 5) {
+                    console.log(wordDict[wordlist[z]])
                     count++;
                 }
+
+
             }
             // wordDict[wordlist[z]].size == 0?count++:null;       
             // console.log('size',wordDict[wordlist[z]].size);
@@ -59,9 +58,5 @@ function spellingBeeSolutions(wordlist, puzzles) {
 //     ['AELWXYZ', 'AELPXYZ', 'AELPSXY', 'SAELPXY', 'XAELPSY']) // [0,1,3,2,0]
 spellingBeeSolutions(
     ['APPLE', 'PLEAS', 'PLEASE'],
-    ['AELWXYZ']) // [0,1,3,2,0]
+    ['AELPSXY']) // [0,1,3,2,0]
 
-// we can do a simple check if all letters in wordlist are in the puzzle
-// check if key letter from puzzle is present in word list
-// check if wordlist is at least five letters long && puzzles are 7 letteres long
-// 'AELPSXY' & 'PLEASE' => PLEASE, 
--- deleteme.js
+++
@@ -1,9 +0,0 @@
-function popMe(arr){
-    console.log(arr)
-    for(var i =  0; i<arr.length; i++){
-        arr.pop()
-        
-    }
-    console.log(arr)
-}
-popMe([1,2,3,4,5])
