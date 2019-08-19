d539790f68b9a7e054086a5851415954f0fc87cd
phongnguyen39 <phong.nguyen39@gmail.com>
2019-08-19 09:29:42 -0700

    added LC- rotate image.js

--- Leetcode - Reverse Linked List.js
+++ Leetcode - Reverse Linked List.js
@@ -1,4 +1,4 @@
-https://leetcode.com/explore/interview/card/top-interview-questions-easy/93/linked-list/560/
+// https://leetcode.com/explore/interview/card/top-interview-questions-easy/93/linked-list/560/
 
 class Node {
     constructor(val){
@@ -15,15 +15,29 @@ class SLL {
 
     append(val){
         let newNode = new Node(val)
+        
         if(!this.head){
             this.head = newNode;
             this.tail = newNode;
             return;
         }
-
+        
         this.tail.next = newNode;
         this.tail = newNode;
         return;
+    }
+
+    reverse(){
+        let currentNode = this.head;
+        let runnerNode = currentNode.next;
+        let temp = runnerNode.next;
+
+        while(temp != null) {
+            console.log(currentNode.val, runnerNode.val, temp.val)
+            runnerNode.next = currentNode;
+            currentNode = currentNode.next;
+            temp = temp.next;
+        }
     }
 
     
@@ -34,7 +48,7 @@ sll.append(1);
 sll.append(2);
 sll.append(3);
 sll.append(4);
-sll.append(5);
+sll.append(5)
 sll.reverse();
 console.log(sll)
 
