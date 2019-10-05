4cdfeadee24fc904ccb65fd46bbebc596aff6a24
phongnguyen39 <phong.nguyen39@gmail.com>
2019-10-04 20:40:23 -0700

    progress point with LC - Unique number of Occurrences.js

--- Leetcode - Unique Number of Occurrences.js
+++ Leetcode - Unique Number of Occurrences.js
@@ -1,1 +1,8 @@
 // https://leetcode.com/problems/unique-number-of-occurrences/
+
+let uniqueOccurrences =(arr)=> {
+    
+}
+uniqueOccurrences([1,2,2,1,1,3]) // true
+uniqueOccurrences([1,2]) // false
+uniqueOccurrences([-3,0,1,-3,1,1,1,-3,10,0]) // true
